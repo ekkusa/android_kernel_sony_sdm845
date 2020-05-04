da0a580927290cf83a691b04cc510053cebf2221
Lau <laststandrighthere@gmail.com>
2020-05-05 01:52:20 +0800

    schedutil: Import use_pelt() and it's usages from msm-4.14
    This function was introduced in commit ("sched: Add schedutil
    snapshot"), which is taken from msm-4.9 as of commit (Revert
    "sched/fair: Limit sync wakeup bias to waker cpu") but ...
    msm-4.9 doesn't have it.
    
    Signed-off-by: Lau <laststandrighthere@gmail.com>
    Signed-off-by: MadeOfGreat <ravenklawasd@gmail.com>

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -145,6 +145,15 @@ static bool sugov_up_down_rate_limit(struct sugov_policy *sg_policy, u64 time,
 	return false;
 }
 
+static inline bool use_pelt(void)
+{
+#ifdef CONFIG_SCHED_WALT
+	return (!sysctl_sched_use_walt_cpu_util || walt_disabled);
+#else
+	return true;
+#endif
+}
+
 static void sugov_update_commit(struct sugov_policy *sg_policy, u64 time,
 				unsigned int next_freq)
 {
@@ -168,8 +177,9 @@ static void sugov_update_commit(struct sugov_policy *sg_policy, u64 time,
 			return;
 
 		policy->cur = next_freq;
-	} else {
-		sg_policy->work_in_progress = true;
+	} else if (!sg_policy->work_in_progress) {
+		if (use_pelt())
+			sg_policy->work_in_progress = true;
 		sched_irq_work_queue(&sg_policy->irq_work);
 	}
 }
@@ -527,6 +537,22 @@ static void sugov_work(struct kthread_work *work)
 {
 	struct sugov_policy *sg_policy = container_of(work, struct sugov_policy, work);
 	unsigned long flags;
+	unsigned int freq;
+
+	/*
+	 * Hold sg_policy->update_lock shortly to handle the case where:
+	 * incase sg_policy->next_freq is read here, and then updated by
+	 * sugov_update_shared just before work_in_progress is set to false
+	 * here, we may miss queueing the new update.
+	 *
+	 * Note: If a work was queued after the update_lock is released,
+	 * sugov_work will just be called again by kthread_work code; and the
+	 * request will be proceed before the sugov thread sleeps.
+	 */
+	freq = sg_policy->next_freq;
+	
+	if (use_pelt())
+		sg_policy->work_in_progress = false;
 
 	mutex_lock(&sg_policy->work_lock);
 	raw_spin_lock_irqsave(&sg_policy->update_lock, flags);
