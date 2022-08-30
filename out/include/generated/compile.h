a349fb73f43533deba3422f74529cf9d8a2b4d0d
ekkusa <dayesofficial@gmail.com>
2022-08-30 17:13:00 +0000

    git-ignore: add out/ to .gitignore
    This is to prevent accidentally adding or commiting the /out directory.

--- out/include/generated/compile.h
+++
@@ -1,7 +0,0 @@
-/* This file is auto generated, version 0 */
-/* SMP PREEMPT */
-#define UTS_MACHINE "aarch64"
-#define UTS_VERSION "#0 SMP PREEMPT Tue Aug 30 16:34:12 UTC 2022"
-#define LINUX_COMPILE_BY "えくそう"
-#define LINUX_COMPILE_HOST "ubuntu"
-#define LINUX_COMPILER "Android (7485623, based on r416183b1) clang version 12.0.7 (https://android.googlesource.com/toolchain/llvm-project c935d99d7cf2016289302412d708641d52d2f7ee)"
