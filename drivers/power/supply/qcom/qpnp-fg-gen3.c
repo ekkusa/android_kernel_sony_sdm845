8f5c28177681f838885cb5b22a76fa1dbe25ca66
ekkusa <dayesofficial@gmail.com>
2022-10-13 00:22:46 +0000

    power/supply: qpnp-fg-gen3: silence fg_charge_full_update

--- drivers/power/supply/qcom/qpnp-fg-gen3.c
+++ drivers/power/supply/qcom/qpnp-fg-gen3.c
@@ -2477,8 +2477,9 @@ static int fg_charge_full_update(struct fg_chip *chip)
 				chip->recharge_counter++;
 				need_monotonic_soc_update = true;
 			} else {
-				fg_dbg(chip, FG_SOMC,
-					"Other case during charge_full.\n");
+				pr_debug(chip, FG_SOMC,
+					"
+Other case during charge_full.\n");
 			}
 		} else if (chip->charge_status ==
 				POWER_SUPPLY_STATUS_CHARGING) {
